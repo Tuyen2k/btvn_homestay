fd3f3afb95ef12150cced14236dadf62034f94b1
Tuyen2k <tuyen4835@gmail.com>
2023-09-26 13:38:19 +0700

    pull dev của thái

--- btvn_homstay FE/js/home.js
+++ btvn_homstay FE/js/home.js
@@ -20,19 +20,20 @@ getAll();
 
 function show(arr) {
     let str = "";
+    let count = 0;
     for (const h of arr) {
         str += ` <tr>
-              <td>${h.id_homestay}</td>
+              <td>${++count}</td>
               <td>${h.name}</td>
               <td>${h.price}</td>
               <td>${h.description}</td>
               <td>${h.max_number_stay}</td>
-              <td>${h.image}</td>   
+              <td><img src="${h.image}" alt="image"> </td>   
               <td>${h.status.name}</td>
-              <td>${h.address.address_detail}</td>
               <td>${h.address.city.name}</td>
               <td>${h.address.district.name}</td>
               <td>${h.address.ward.name}</td>
+              <td>${h.address.address_detail}</td>
               <td><button type="button" class="btn btn-warning" onclick="showEdit(${h.id_homestay})" data-toggle="modal" data-target="#modalEdit" >Edit</button></td>
               <td><button type="button" class="btn btn-danger"  onclick="deleteS(${h.id_homestay})">Delete</button></td>
              </tr>`
