790d79f5afa335c86ae954b824223fa10d684724
danghoan95 <hoanvd2210@gmail.com>
2023-09-27 10:11:07 +0700

    status

--- btvn_homstay FE/js/CreateHomestay.js
+++ btvn_homstay FE/js/CreateHomestay.js
@@ -17,7 +17,12 @@ function status() {
 
 function getDataDB() {
     status();
+    findCity();
+    // findDistrict();
+    // findWard();
     address();
+    
+    
 }
 
 function address() {
@@ -80,5 +85,62 @@ function createHomestay() {
     event.preventDefault();
 }
 
+function findCity(){
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/city",
+        success: function(data){
+            let content = "";
+            for (let i = 0; i < data.length; i++) {
+                content += `<option value=${data[i].id_city}>${data[i].name}</option>`;
+            }
+            document.getElementById("select_city").innerHTML = content;
+        }
+    })
+}
+function findDistrict(){
 
-        
+    $(document).ready(function () {
+        $('select').selectize({
+            sortField: 'text'
+        });
+    });
+    let id_city = document.getElementById("city").value;
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/api/districts/${id_city}/city`,
+        success: function(data){
+            let content = '<select class="select" onchange="findWard()" id="district">';
+            content += '<option>Choice a district...</option>';
+            for (let i = 0; i < data.length; i++) {
+                content += '<option value="'+data[i].id_district+'">'+data[i].name+'</option>';
+            }
+            content += "</select>";
+            document.getElementById("select_district").innerHTML = content;
+        }
+    })
+}
+function findWard(){
+
+    $(document).ready(function () {
+        $('select').selectize({
+            sortField: 'text'
+        });
+    });
+    let id_district = document.getElementById("district").value;
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/api/wards/${id_district}/district`,
+        success: function(data){
+            let content = '<select class="select" id="ward">';
+            content += '<option>Choice a ward...</option>';
+            for (let i = 0; i < data.length; i++) {
+                content += '<option value="'+data[i].id_ward+'">'+data[i].name+'</option>';
+            }
+            content += "</select>";
+            document.getElementById("select_ward").innerHTML = content;
+        }
+    })
+}
+
+
